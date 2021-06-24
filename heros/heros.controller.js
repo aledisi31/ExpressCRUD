83e8057017b2f617abee477f750cbe61a6538a29
aledisi31 <86404076+aledisi31@users.noreply.github.com>
2021-06-24 10:33:17 +0200

    Initial commit

---
+++ heros/heros.controller.js
@@ -0,0 +1,49 @@
+var Heros = require('./heros.dao');
+
+exports.createHero = function (req, res, next) {
+    var hero = {
+        name: req.body.name,
+        description: req.body.description
+    };
+    Heros.create(hero, function(err, hero) {
+        if(err) {
+            res.json({error: err})
+        }
+        res.json({message: "hero created successfully"})
+    })
+}
+
+exports.getHeros = function (req, res, next) {
+    Heros.getMaxListeners({}, function (err, heros) {
+        if(err) {
+            res.json({error: err})
+        }
+        res.json({heros: heros})
+    })
+}
+
+exports.getHero = function (req, res, next) {
+    Heros.get ({name: req.params.name}, function(err, heros) {
+        if(err) {
+            res.json ({error: err})
+        }
+        res.json ({heros: heros})
+    })
+}
+
+exports.updateHero = function (req, res, next) {
+    var hero = {
+        name: req.body.name,
+        description: req.body.description
+    }
+    Heros.updateOne({_id: req.params.id})
+}
+
+exports.removeHero = function (req, res, next) {
+    Heros.deleteOne({_id: req.params.id}, function (err, hero) {
+        if (err) {
+            res.json({error: err})
+        }
+        res.json ({message: "Hero deleted successfully"})
+    })
+}
